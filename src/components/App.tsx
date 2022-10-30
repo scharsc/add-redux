7e74058d09eb86114273d3f927acf7cc48d89be2
Harald Schmid <scharsc@googlemail.com>
2022-10-30 13:01:47 +0100

    initializing repo

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LevelSelectorComboxBox from "./LevelSelectorComboBox";
+
+const App = () => {
+    return (
+      <>
+        <br/>
+        <LevelSelectorComboxBox/>
+      </>
+    );
+  };
+  
+  export default LevelSelectorComboxBox;
